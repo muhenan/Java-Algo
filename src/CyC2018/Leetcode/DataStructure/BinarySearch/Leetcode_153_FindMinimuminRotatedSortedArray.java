cce4b1ce6ac128af0aa080ecb1fd578cc993f7ca
muhenan <1149423577@qq.com>
2022-02-27 19:19:05 +0800

    153

---
+++ src/CyC2018/Leetcode/DataStructure/BinarySearch/Leetcode_153_FindMinimuminRotatedSortedArray.java
@@ -0,0 +1,17 @@
+package CyC2018.Leetcode.DataStructure.BinarySearch;
+
+// 简单的二分查找，最后 low 和 high 走到一个低的小数组里 返回 nums[low]
+
+public class Leetcode_153_FindMinimuminRotatedSortedArray {
+    public int findMin(int[] nums) {
+        int length = nums.length;
+        int low = 0;
+        int high = length - 1;
+        while (nums[low] > nums[high]) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] > nums[high]) low = mid + 1;
+            else high = mid;
+        }
+        return nums[low];
+    }
+}
