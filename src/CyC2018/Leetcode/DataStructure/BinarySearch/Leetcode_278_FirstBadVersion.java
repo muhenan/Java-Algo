926e1549634f7427871b4ba8fbe7c13c94781e6f
muhenan <1149423577@qq.com>
2022-02-27 19:11:13 +0800

    278

---
+++ src/CyC2018/Leetcode/DataStructure/BinarySearch/Leetcode_278_FirstBadVersion.java
@@ -0,0 +1,19 @@
+package CyC2018.Leetcode.DataStructure.BinarySearch;
+
+public class Leetcode_278_FirstBadVersion {
+
+    private boolean isBadVersion(int version) {
+        return false;
+    }
+
+    public int firstBadVersion(int n) {
+        int low = 1;
+        int high = n;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (!isBadVersion(mid)) low = mid + 1;
+            else high = mid;
+        }
+        return low;
+    }
+}
