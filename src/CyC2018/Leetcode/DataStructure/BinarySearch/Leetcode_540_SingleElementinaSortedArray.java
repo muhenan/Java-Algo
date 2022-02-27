12457614457baa9f20834b47b6e37fc1270a0831
muhenan <1149423577@qq.com>
2022-02-27 19:02:18 +0800

    540

---
+++ src/CyC2018/Leetcode/DataStructure/BinarySearch/Leetcode_540_SingleElementinaSortedArray.java
@@ -0,0 +1,28 @@
+package CyC2018.Leetcode.DataStructure.BinarySearch;
+
+/**
+ * 最后返回的那个，肯定是第奇数，因为前面肯定有 k 个 2 连组了
+ *
+ * 不难，一下就做对了
+ * 二分查找的思想，结合 index 和前后数的情况，只要知道答案在哪边，要往哪边走就行了
+ * **/
+
+public class Leetcode_540_SingleElementinaSortedArray {
+    public int singleNonDuplicate(int[] nums) {
+        int length = nums.length;
+        if (length == 1) return nums[0];
+        int low = 0;
+        int high = length - 1;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+             if (mid % 2 == 0) {
+                 if (nums[mid] == nums[mid + 1]) low = mid + 1;
+                 else high = mid;
+             } else {
+                 if (nums[mid] == nums[mid - 1]) low = mid + 1;
+                 else high = mid;
+             }
+        }
+        return nums[low];
+    }
+}
