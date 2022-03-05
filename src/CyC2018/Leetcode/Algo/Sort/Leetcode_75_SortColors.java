73fefa4aedf92c1c4a604d0dae7a068c708e5dbf
muhenan <1149423577@qq.com>
2022-03-05 15:35:52 +0800

    color sort

--- src/CyC2018/Leetcode/Algo/Sort/Leetcode_75_SortColors.java
+++ src/CyC2018/Leetcode/Algo/Sort/Leetcode_75_SortColors.java
@@ -60,6 +60,12 @@ public class Leetcode_75_SortColors {
             }
         }
     }
+    // 三指针的方法
+    // 0 和 2 都是默认已经指着 0 和 2 了
+    // 假设最左边出来一个是 0，假设最右边出来一个是 nums.length，这种在边界添一个的假设方法非常常用
+    // 然后 换的时候，0 和 2都是先走一步再换
+    // 和 0 换的时候 1 换完也走一步
+    // 和 2 换的时候 换到 不是2为止，1才往前走
 
     public static void main(String[] args) {
         Leetcode_75_SortColors solu = new Leetcode_75_SortColors();
