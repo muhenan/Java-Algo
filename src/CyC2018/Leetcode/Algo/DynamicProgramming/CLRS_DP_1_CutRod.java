96e8e031e3c532982a7f00ca7938723bb39836e4
muhenan <1149423577@qq.com>
2022-04-19 22:18:10 +0800

    rod cut init

--- src/CyC2018/Leetcode/Algo/DynamicProgramming/CLRS_DP_1_CutRod.java
+++ src/CyC2018/Leetcode/Algo/DynamicProgramming/CLRS_DP_1_CutRod.java
@@ -1,4 +1,14 @@
 package CyC2018.Leetcode.Algo.DynamicProgramming;
 
+/**
+ *
+ * 本次介绍一个非常经典的问题，算法导论 CLRS 中动态规划这一章里的第一个题
+ * Cut Rod 钢条切割问题
+ *
+ * （该题非常经典，01背包等等问题都是在在此题基础之上）
+ *
+ * **/
+
 public class CLRS_DP_1_CutRod {
+
 }
