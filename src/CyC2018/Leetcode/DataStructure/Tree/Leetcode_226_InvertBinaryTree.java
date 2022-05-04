fdc8c6681af80ff37d41c0f78a2cea363245aae6
muhenan <1149423577@qq.com>
2022-05-04 20:24:32 +0800

    invert

---
+++ src/CyC2018/Leetcode/DataStructure/Tree/Leetcode_226_InvertBinaryTree.java
@@ -0,0 +1,4 @@
+package CyC2018.Leetcode.DataStructure.Tree;
+
+public class Leetcode_226_InvertBinaryTree {
+}
---
+++ src/CyC2018/Leetcode/DataStructure/Tree/Leetcode_543_DiameterofBinaryTree.java
@@ -0,0 +1,20 @@
+package CyC2018.Leetcode.DataStructure.Tree;
+
+/**
+ * 我认为依旧是最大深度的变形，只是这次要在每个节点加上统计左右树深度相加的值
+ * 并且记录这个值的最大值
+ * */
+public class Leetcode_543_DiameterofBinaryTree {
+    private int diameter = 0; // 关于这种记录的问题，依旧使用一个全局变量来记录
+    public int diameterOfBinaryTree(TreeNode root) {
+        maxDepth(root);
+        return diameter;
+    }
+    private int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        int depthOfLeft = maxDepth(root.left);
+        int depthOfRight = maxDepth(root.right);
+        diameter = Math.max(diameter, depthOfLeft + depthOfRight);
+        return Math.max(depthOfLeft, depthOfRight) + 1;
+    }
+}
