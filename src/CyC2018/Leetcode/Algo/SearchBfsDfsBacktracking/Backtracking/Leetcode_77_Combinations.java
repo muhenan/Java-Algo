21ada441476a83c601568866fee4e329482b8178
muhenan <1149423577@qq.com>
2022-08-17 16:08:28 +0800

    easy2

---
+++ src/CyC2018/Leetcode/Algo/SearchBfsDfsBacktracking/Backtracking/Leetcode_77_Combinations.java
@@ -0,0 +1,17 @@
+package CyC2018.Leetcode.Algo.SearchBfsDfsBacktracking.Backtracking;
+
+import java.util.List;
+
+/**
+ *
+ * 全排列的简化，比全排列还简单，只是换一下判断 tempOne 的长度
+ *
+ * 比较 easy，这里选择不写了
+ *
+ * */
+
+public class Leetcode_77_Combinations {
+    public List<List<Integer>> combine(int n, int k) {
+        return null;
+    }
+}
