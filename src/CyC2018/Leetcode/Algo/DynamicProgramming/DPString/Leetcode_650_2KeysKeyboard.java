c2b8a11edfd8135e3b7036d3b2c6697f1113701c
muhenan <1149423577@qq.com>
2022-10-06 19:20:57 -0400

    dp string 2 over

--- src/CyC2018/Leetcode/Algo/DynamicProgramming/DPString/Leetcode_650_2KeysKeyboard.java
+++ src/CyC2018/Leetcode/Algo/DynamicProgramming/DPString/Leetcode_650_2KeysKeyboard.java
@@ -30,6 +30,16 @@ public class Leetcode_650_2KeysKeyboard {
     /**
      * 下面的方法类似于 sqrt(n) * sqrt(n) ?，时间效率提高了
      *
+     * 无敌！！！
+     * 找规律
+     * 只要是质数那么只能一个一个拼装，1，3,5,7,11等
+     * 出去质数，其他数字只要能分解出来，那么分解出来的数相加就是结果。例如 18 = 233,那么结果就是2+3+3
+     *
+     * 作者：ityou-o
+     * 链接：https://leetcode.cn/problems/2-keys-keyboard/solution/javascriptban-jie-ti-si-lu-by-ityou-o-tbys/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     *
      * 大于 sqrt(n) 的时候就没意义了，因为那样的情况一定已经找过了，会造成重复计算
      *
      * 找的就是这种能乘除开的，这样才能全部全部的复制粘贴
